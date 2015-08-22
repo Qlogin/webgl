33d2105120c544b34ca9ace11e7287e0cf634f03
Roman Kulagin <Roman.Qlogin@gmail.com>
2015-08-22 14:45:26 +0300

    Refactoring: separate primitives, update controls

--- geometricCAD.js
+++ geometricCAD.js
@@ -51,12 +51,6 @@ window.onload = function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     //
-    //  Initialize our data for the Sierpinski Gasket
-    //
-
-    // First, initialize the corners of our gasket with three points.
-
-    //
     //  Configure WebGL
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -123,21 +117,31 @@ window.onload = function init()
 
         $('#course').val(camera.course.toFixed());
         $('#pitch').val(camera.pitch.toFixed());
+        $('output').filter( function( index, el ) {
+            return $( this ).attr( "for" ) === "course";
+        }).val(camera.course.toFixed());
+        $('output').filter( function( index, el ) {
+            return $( this ).attr( "for" ) === "pitch";
+        }).val(camera.pitch.toFixed());
 
         camera.updateView();
         render();
     });
 
     canvas.addEventListener('wheel', function(event) {
-        camera.distance += event.deltaY / 5;
+        camera.distance += (event.deltaX || event.deltaY) < 0 ? -0.2 : 0.2;
         if (camera.distance < 1)
            camera.distance = 1;
-        else if (camera.distance > 10)
-           camera.distance = 10;
+        else if (camera.distance > 30)
+           camera.distance = 30;
         $('#distance').val(camera.distance);
+        $('output').filter( function( index, el ) {
+            return $( this ).attr( "for" ) === "distance";
+        }).val(camera.distance.toFixed(1));
+
         camera.updateView();
         render();
-        event.stopPropagation();
+        event.preventDefault();
     });
 
     // Create button
@@ -146,21 +150,21 @@ window.onload = function init()
         var prim;
         if (type == 0)
         {
-           prim = createSphere($('#sphere-radius')[0].valueAsNumber,
-                               $('#sphere-hor-subdiv')[0].valueAsNumber,
-                               $('#sphere-vert-subdiv')[0].valueAsNumber);
+           prim = Sphere($('#sphere-radius')[0].valueAsNumber,
+                         $('#sphere-hor-subdiv')[0].valueAsNumber,
+                         $('#sphere-vert-subdiv')[0].valueAsNumber);
         }
         else if (type == 1)
         {
-           prim = createCylinder($('#cylinder-radius')[0].valueAsNumber,
-                                 $('#cylinder-height')[0].valueAsNumber,
-                                 $('#cylinder-subdiv')[0].valueAsNumber);
+           prim = Cylinder($('#cylinder-radius')[0].valueAsNumber,
+                           $('#cylinder-height')[0].valueAsNumber,
+                           $('#cylinder-subdiv')[0].valueAsNumber);
         }
         else if (type == 2)
         {
-           prim = createCone($('#cone-radius')[0].valueAsNumber,
-                             $('#cone-height')[0].valueAsNumber,
-                             $('#cone-subdiv')[0].valueAsNumber);
+           prim = Cone($('#cone-radius')[0].valueAsNumber,
+                       $('#cone-height')[0].valueAsNumber,
+                       $('#cone-subdiv')[0].valueAsNumber);
         }
         else
            return;
@@ -172,7 +176,7 @@ window.onload = function init()
                    $('#rot-y')[0].valueAsNumber,
                    $('#rot-z')[0].valueAsNumber];
         var color = hexToRGB($('#color')[0].value);
-        addObject(pos, rot, color, prim);
+        addObject(prim, pos, rot, color);
         render();
     });
 
@@ -197,206 +201,39 @@ window.onload = function init()
         render();
     });
 
-    $('#sel-pos-x, #sel-pos-y, #sel-pos-z').bind('change', function() {
-        objects[sel_id].pos = [$('#sel-pos-x')[0].valueAsNumber,
-                               $('#sel-pos-y')[0].valueAsNumber,
-                               $('#sel-pos-z')[0].valueAsNumber];
+    $('#sel-pos-x, #sel-pos-y, #sel-pos-z').on('spin spinchange', function() {
+        objects[sel_id].pos = vec3($('#sel-pos-x').spinner("value"),
+                                   $('#sel-pos-y').spinner("value"),
+                                   $('#sel-pos-z').spinner("value"));
         objects[sel_id].update_transform();
         render();
     });
 
-    $('#sel-rot-x, #sel-rot-y, #sel-rot-z').bind('change', function() {
-        objects[sel_id].rot = [$('#sel-rot-x')[0].valueAsNumber,
-                               $('#sel-rot-y')[0].valueAsNumber,
-                               $('#sel-rot-z')[0].valueAsNumber];
+    $('#sel-rot-x, #sel-rot-y, #sel-rot-z').bind('spin spinchange', function() {
+        objects[sel_id].rot = vec3($('#sel-rot-x').spinner("value"),
+                                   $('#sel-rot-y').spinner("value"),
+                                   $('#sel-rot-z').spinner("value"));
         objects[sel_id].update_transform();
         render();
     });
 
-    addObject([0, 0, 1], [0, 0, 0], [1, 0, 0], createSphere(1, 16, 12));
-    addObject([1.5, -1.5, 0.5], [0, -30, 0], [0, 1, 0], createCylinder(0.5, 2, 15));
+    addObject(Sphere(1, 16, 12), [0, 0, 1], [0, 0, 0], [1, 0, 0]);
+    addObject(Cylinder(0.5, 2, 15), [1.5, -1.5, 0.5], [0, -30, 0], [0, 1, 0]);
 
     render();
 };
 
-function addObject(position, orientation, color, primitive)
+function addObject(obj, position, orientation, color)
 {
-    var obj = new Object();
     obj.pos   = position;
     obj.rot   = orientation;
     obj.color = color;
-
-    obj.update_transform = function() {
-        var tr = translate(this.pos);
-        var rot_x = rotate(this.rot[0], 1, 0, 0);
-        var rot_y = rotate(this.rot[1], 0, 1, 0);
-        var rot_z = rotate(this.rot[2], 0, 0, 1);
-        this.transform = mult(mult(tr, rot_z), mult(rot_y, rot_x));
-    }
     obj.update_transform();
-
-    obj.vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, obj.vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(primitive.points), gl.STATIC_DRAW );
-
-    obj.triBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, obj.triBuffer );
-    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(primitive.tris), gl.STATIC_DRAW);
-    obj.triNum = primitive.tris.length;
-
-    obj.lineBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, obj.lineBuffer );
-    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(primitive.lines), gl.STATIC_DRAW);
-    obj.lineNum = primitive.lines.length;
 
     objects.push(obj);
 
-    var full_name = primitive.name + ' #' + objects.length;
+    var full_name = obj.type + ' #' + objects.length;
     $('#obj-list').append("<option value='" + (objects.length - 1) + "'>" + full_name + "</option>");
-}
-
-function createSphere(r, hnum, vnum)
-{
-    var sphere = new Object();
-    sphere.name = "Sphere";
-    sphere.points = [];
-
-    var i, j;
-    for (i = 0; i <= vnum; ++i)
-    {
-        var p = Math.PI / 2 - Math.PI * i / vnum;
-        for (j = 0; j != hnum; ++j)
-        {
-            var c = 2 * Math.PI * j / hnum;
-            sphere.points.push(vec3(r * Math.cos(p) * Math.cos(c),
-                                    r * Math.cos(p) * Math.sin(c),
-                                    r * Math.sin(p)));
-        }
-    }
-
-    sphere.tris = [];
-    for (i = 0; i != vnum; ++i)
-        for (j = 0; j != hnum; ++j)
-        {
-           sphere.tris.push(i * hnum + j);
-           sphere.tris.push(i * hnum + (j + 1) % hnum);
-           sphere.tris.push((i + 1) * hnum + (j + 1) % hnum);
-           sphere.tris.push((i + 1) * hnum + (j + 1) % hnum);
-           sphere.tris.push((i + 1) * hnum + j);
-           sphere.tris.push(i * hnum + j);
-        }
-
-    sphere.lines = [];
-    for (i = 0; i != vnum; ++i)
-        for (j = 0; j != hnum; ++j)
-        {
-           sphere.lines.push(i * hnum + j);
-           sphere.lines.push(i * hnum + (j + 1) % hnum);
-           sphere.lines.push(i * hnum + j);
-           sphere.lines.push((i + 1) * hnum + j);
-        }
-
-    return sphere;
-}
-
-function createCylinder(r, h, hnum)
-{
-    var cylinder = new Object();
-    cylinder.name = "Cylinder";
-    cylinder.points = [];
-
-    var i;
-    for (i = 0; i != hnum; ++i)
-    {
-       var c = 2 * Math.PI * i / hnum;
-       cylinder.points.push(vec3(r * Math.cos(c), r * Math.sin(c), h/2));
-       cylinder.points.push(vec3(r * Math.cos(c), r * Math.sin(c), -h/2));
-    }
-    var vcap = cylinder.points.length;
-    cylinder.points.push(vec3(0, 0, h/2));
-    cylinder.points.push(vec3(0, 0, -h/2));
-
-    cylinder.tris = [];
-    for (i = 0; i != hnum; ++i)
-    {
-        // Side
-        cylinder.tris.push(2 * i);
-        cylinder.tris.push(2 * ((i + 1) % hnum));
-        cylinder.tris.push(2 * ((i + 1) % hnum) + 1);
-        cylinder.tris.push(2 * ((i + 1) % hnum) + 1);
-        cylinder.tris.push(2 * i + 1);
-        cylinder.tris.push(2 * i);
-
-        // Cap
-        cylinder.tris.push(vcap);
-        cylinder.tris.push(2 * i);
-        cylinder.tris.push(2 * ((i + 1) % hnum));
-        cylinder.tris.push(vcap + 1);
-        cylinder.tris.push(2 * i + 1);
-        cylinder.tris.push(2 * ((i + 1) % hnum) + 1);
-    }
-
-    cylinder.lines = [];
-    for (i = 0; i != hnum; ++i)
-    {
-        // Side
-        cylinder.lines.push(2 * ((i + 1) % hnum));
-        cylinder.lines.push(2 * i);
-        cylinder.lines.push(2 * i);
-        cylinder.lines.push(2 * i + 1);
-        cylinder.lines.push(2 * i + 1);
-        cylinder.lines.push(2 * ((i + 1) % hnum) + 1);
-
-        // Cap
-        cylinder.lines.push(vcap);
-        cylinder.lines.push(2 * i);
-        cylinder.lines.push(vcap + 1);
-        cylinder.lines.push(2 * i + 1);
-    }
-    return cylinder;
-}
-
-function createCone(r, h, hnum)
-{
-    var cone = new Object();
-    cone.name = "Cone";
-    cone.points = [];
-
-    var i;
-    for (i = 0; i != hnum; ++i)
-    {
-       var c = 2 * Math.PI * i / hnum;
-       cone.points.push(vec3(r * Math.cos(c), r * Math.sin(c), 0));
-    }
-    var vcap = cone.points.length;
-    cone.points.push(vec3(0, 0, h));
-    cone.points.push(vec3(0, 0, 0));
-
-    cone.tris = [];
-    for (i = 0; i != hnum; ++i)
-    {
-        // Side
-        cone.tris.push(vcap);
-        cone.tris.push(i);
-        cone.tris.push((i + 1) % hnum);
-
-        // Cap
-        cone.tris.push(vcap + 1);
-        cone.tris.push(i);
-        cone.tris.push((i + 1) % hnum);
-    }
-
-    cone.lines = [];
-    for (i = 0; i != hnum; ++i)
-    {
-        cone.lines.push(i);
-        cone.lines.push(vcap);
-        cone.lines.push(i);
-        cone.lines.push(vcap + 1);
-        cone.lines.push(i);
-        cone.lines.push((i + 1) % hnum);
-    }
-    return cone;
 }
 
 function render()
---
+++ primitives.js
@@ -0,0 +1,231 @@
+"use strict";
+
+var gl;
+
+function Primitive(t) {
+    return {
+        pos   : vec3(),
+        rot   : vec3(),
+        color : vec3(1, 1, 1),
+        transform : mat4(),
+        type      : t,
+        has_lines : true,
+        has_tris  : true,
+        params    : {},
+
+        update_transform : function() {
+            var tr = translate(this.pos);
+            var rot_x = rotate(this.rot[0], 1, 0, 0);
+            var rot_y = rotate(this.rot[1], 0, 1, 0);
+            var rot_z = rotate(this.rot[2], 0, 0, 1);
+            this.transform = mult(mult(tr, rot_z), mult(rot_y, rot_x));
+        },
+
+        vBuffer    : gl.createBuffer(),
+        triBuffer  : gl.createBuffer(),
+        lineBuffer : gl.createBuffer(),
+
+        update_buffers : function() {
+            var points = this.get_points();
+            gl.bindBuffer( gl.ARRAY_BUFFER, this.vBuffer );
+            gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.DYNAMIC_DRAW );
+
+            if (this.has_tris) {
+                var tris = this.get_triangles();
+                gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.triBuffer );
+                gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(tris), gl.DYNAMIC_DRAW);
+                this.triNum = tris.length;
+            }
+
+            if (this.has_lines) {
+                var lines = this.get_lines();
+                gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.lineBuffer );
+                gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(lines), gl.DYNAMIC_DRAW);
+                this.lineNum = lines.length;
+            }
+        }
+    };
+}
+
+function Sphere(r, hnum, vnum)
+{
+    var sphere = Primitive("Sphere");
+    sphere.r = r;
+    sphere.hnum = hnum;
+    sphere.vnum = vnum;
+    sphere.get_points = function() {
+        var i, j;
+        var points = [];
+        for (i = 0; i <= this.vnum; ++i)
+        {
+            var p = Math.PI / 2 - Math.PI * i / this.vnum;
+            for (j = 0; j != this.hnum; ++j)
+            {
+                var c = 2 * Math.PI * j / this.hnum;
+                points.push(vec3(r * Math.cos(p) * Math.cos(c),
+                                 r * Math.cos(p) * Math.sin(c),
+                                 r * Math.sin(p)));
+            }
+        }
+        return points;
+    };
+
+    sphere.get_triangles = function() {
+        var indices = [];
+        var i, j;
+        for (i = 0; i != this.vnum; ++i)
+            for (j = 0; j != this.hnum; ++j)
+            {
+               indices.push(i * this.hnum + j);
+               indices.push(i * this.hnum + (j + 1) % this.hnum);
+               indices.push((i + 1) * this.hnum + (j + 1) % this.hnum);
+               indices.push((i + 1) * this.hnum + (j + 1) % this.hnum);
+               indices.push((i + 1) * this.hnum + j);
+               indices.push(i * this.hnum + j);
+            }
+        return indices;
+    };
+
+    sphere.get_lines = function() {
+        var indices = [];
+        var i, j;
+        for (i = 0; i != vnum; ++i)
+            for (j = 0; j != hnum; ++j)
+            {
+               indices.push(i * hnum + j);
+               indices.push(i * hnum + (j + 1) % hnum);
+               indices.push(i * hnum + j);
+               indices.push((i + 1) * hnum + j);
+            }
+        return indices;
+    };
+    sphere.update_buffers();
+    return sphere;
+}
+
+function Cylinder(r, h, hnum)
+{
+    var cylinder = Primitive("Cylinder");
+    cylinder.r = r;
+    cylinder.h = h;
+    cylinder.hnum = hnum;
+
+    cylinder.get_points = function() {
+        var points = [];
+        var i;
+        for (i = 0; i != this.hnum; ++i)
+        {
+           var c = 2 * Math.PI * i / this.hnum;
+           points.push(vec3(this.r * Math.cos(c), this.r * Math.sin(c), this.h/2));
+           points.push(vec3(this.r * Math.cos(c), this.r * Math.sin(c), -this.h/2));
+        }
+        points.push(vec3(0, 0, this.h/2));
+        points.push(vec3(0, 0, -this.h/2));
+        return points;
+    }
+
+    cylinder.get_triangles = function() {
+        var indices = [];
+        var vcap = 2 * this.hnum;
+        var i;
+        for (i = 0; i != this.hnum; ++i)
+        {
+            // Side
+            indices.push(2 * i);
+            indices.push(2 * ((i + 1) % this.hnum));
+            indices.push(2 * ((i + 1) % this.hnum) + 1);
+            indices.push(2 * ((i + 1) % this.hnum) + 1);
+            indices.push(2 * i + 1);
+            indices.push(2 * i);
+
+            // Cap
+            indices.push(vcap);
+            indices.push(2 * i);
+            indices.push(2 * ((i + 1) % this.hnum));
+            indices.push(vcap + 1);
+            indices.push(2 * i + 1);
+            indices.push(2 * ((i + 1) % this.hnum) + 1);
+        }
+        return indices;
+    };
+
+    cylinder.get_lines = function() {
+        var indices = [];
+        var vcap = 2 * this.hnum;
+        var i;
+        for (i = 0; i != hnum; ++i)
+        {
+            // Side
+            indices.push(2 * ((i + 1) % this.hnum));
+            indices.push(2 * i);
+            indices.push(2 * i);
+            indices.push(2 * i + 1);
+            indices.push(2 * i + 1);
+            indices.push(2 * ((i + 1) % this.hnum) + 1);
+
+            // Cap
+            indices.push(vcap);
+            indices.push(2 * i);
+            indices.push(vcap + 1);
+            indices.push(2 * i + 1);
+        }
+        return indices;
+    };
+    cylinder.update_buffers();
+    return cylinder;
+}
+
+function Cone(r, h, hnum)
+{
+    var cone = Primitive("Cone");
+    cone.r = r;
+    cone.h = h;
+    cone.hnum = hnum;
+
+    cone.get_points = function() {
+        var points = [];
+        var i;
+        for (i = 0; i != this.hnum; ++i)
+        {
+           var c = 2 * Math.PI * i / this.hnum;
+           cone.points.push(vec3(this.r * Math.cos(c), r * Math.sin(c), 0));
+        }
+        points.push(vec3(0, 0, this.h));
+        points.push(vec3(0, 0, 0));
+        return points;
+    }
+
+    cone.get_triangles = function() {
+        var indices = [];
+        var i;
+        for (i = 0; i != this.hnum; ++i)
+        {
+            // Side
+            indices.push(this.hnum);
+            indices.push(i);
+            indices.push((i + 1) % this.hnum);
+
+            // Cap
+            indices.push(this.hnum + 1);
+            indices.push(i);
+            indices.push((i + 1) % this.hnum);
+        }
+        return indices;
+    };
+
+    cone.get_lines = function() {
+        var indices = [];
+        for (i = 0; i != this.hnum; ++i)
+        {
+            indices.push(i);
+            indices.push(this.hnum);
+            indices.push(i);
+            indices.push(this.hnum + 1);
+            indices.push(i);
+            indices.push((i + 1) % this.hnum);
+        }
+        return indices;
+    };
+    cone.update_buffers();
+    return cone;
+}
